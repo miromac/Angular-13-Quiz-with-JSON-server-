fb266277f3c454ec0e0eab559c64a0ec73777914
Miro Mac <itcodevelopment@gmail.com>
2022-02-12 15:56:55 +0200

    5. Template Binding of Quiz Page

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -15,10 +15,15 @@ export class QuizComponent implements OnInit {
     this.quizService.getQuestions().then((res)=>{
       console.log(res);
       this.quizService.questionsData = res.results;
+      this.quizService.questionsData[this.questionIndex].incorrect_answers.push(this.quizService.questionsData[this.questionIndex].correct_answer);
+      this.quizService.questionsData[this.questionIndex].incorrect_answers.sort(() => 0.5 - Math.random());
+      
     }).catch((err)=>{
       console.log(err);
     });
 
   }
+
+  questionIndex = 0;
 
 }
