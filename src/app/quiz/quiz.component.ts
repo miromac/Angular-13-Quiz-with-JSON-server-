c8536d51046a705eff1c3d059368bbfe03c04ff3
Miro Mac <itcodevelopment@gmail.com>
2022-02-12 16:34:58 +0200

    6. Logic for Answering the Questions

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { QuizService } from '../services/quiz.service';
 
 @Component({
@@ -8,7 +9,7 @@ import { QuizService } from '../services/quiz.service';
 })
 export class QuizComponent implements OnInit {
 
-  constructor(public quizService:QuizService) { }
+  constructor(public quizService:QuizService, private router:Router) { }
 
   ngOnInit(): void {
 
@@ -25,5 +26,29 @@ export class QuizComponent implements OnInit {
   }
 
   questionIndex = 0;
+  userAnswer = "";
+  score = 0;
+
+  selectAnswer(option: string) {
+    this.userAnswer = option;
+    
+    if (this.quizService.questionsData[this.questionIndex].correct_answer == option)
+    {
+      this.score += 5;
+    
+      setTimeout(()=>{
+        this.userAnswer = "";
+        this.questionIndex++;
+        this.quizService.questionsData[this.questionIndex].incorrect_answers.push(this.quizService.questionsData[this.questionIndex].correct_answer);
+        this.quizService.questionsData[this.questionIndex].incorrect_answers.sort(() => 0.5 - Math.random());
+        
+      }, 2000);
+    }
+    else {
+      setTimeout(()=>{
+        this.router.navigate(['/home']);
+      }, 2000);
+    }
+  }
 
 }
